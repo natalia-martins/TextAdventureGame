5855da66c26c7ff5b6f714b554974dd67fea83eb
be3013 <57907227+be3013@users.noreply.github.com>
2024-11-21 22:55:46 -0300

    Add files via upload

--- ai_interaction.py
+++ ai_interaction.py
@@ -1,18 +1,12 @@
 from openai import OpenAI
+
 client = OpenAI()
 
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a very descriptive narrator."
-        },
-        {
-            "role": "user",
-            "content": "Write the start of a medieval adventure with a lone main character."
-        }
-    ]
-)
 
-print(completion.choices[0].message)
+def ai_request(input_array):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=input_array
+    )
+
+    return completion.choices[0].message
